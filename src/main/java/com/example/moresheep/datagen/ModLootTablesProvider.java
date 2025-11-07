17004be8125091a262072beebd2e121b4957407b
Tonyqwe <2419231380@qq.com>
2025-11-07 22:13:02 +0800

    datagenerator

---
+++ src/main/java/com/example/moresheep/datagen/ModLootTablesProvider.java
@@ -0,0 +1,59 @@
+package com.example.moresheep.datagen;
+
+
+
+import com.example.moresheep.block.ModBlocks;
+import com.example.moresheep.item.ModItems;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+
+import java.util.Set;
+
+//战利品列表生成
+public class ModLootTablesProvider extends BlockLootSubProvider {
+
+    public ModLootTablesProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        //dropSelf(ModBlocks.STAR_POINT_BLOCK.get());
+        add(ModBlocks.STAR_POINT_BLOCK.get(),
+                block -> createCopperOreLikeDrops(ModBlocks.STAR_POINT_BLOCK.get(), ModItems.STAR_POINT.get(),2.0f,5.0f));
+
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+
+
+    protected LootTable.Builder createCopperOreLikeDrops(Block block, Item item,float min,float max) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(
+                block,
+                (LootPoolEntryContainer.Builder<?>)this.applyExplosionDecay(
+                        block,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(min, max)))
+                                .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+    }
+
+}
